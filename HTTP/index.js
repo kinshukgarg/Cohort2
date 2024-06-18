7c53f813a584ef25b56810ddc039709ddbd988a0
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-06-18 11:27:38 -0700

    Hello

---
+++ HTTP/index.js
@@ -0,0 +1,11 @@
+// writting Hello world
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(port)
+ 
