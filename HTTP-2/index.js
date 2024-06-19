9056b0b7ff66c2209f110f685d08a3c58ae7c327
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-06-19 08:42:38 -0700

    Hello World

---
+++ HTTP-2/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParse=require('body-parser')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log(req.body )
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
